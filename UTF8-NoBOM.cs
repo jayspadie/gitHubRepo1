25f617e5ea0d22e51c6d27f16598c367dbedea7c
Jay Spadie <jayspadie@users.noreply.github.com>
2019-01-29 14:13:05 -0500

    Files with Chinese encoded as GB18030 and UTF-8 without BOM

---
+++ UTF8-NoBOM.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Class1
+{
+    /// <summary>
+    /// Chinese (Traditional, Hong Kong, SAR): 冔繡楷 勗 施 比憶
+    /// Chinese (Simplified, China): 其加锕 嘛上の 向腌
+    /// </summary>
+	public Class1()
+	{
+	}
+}
